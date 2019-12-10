c6a75b060a6ef3b16d7d510f527ad770530947d8
Ben Leggiero <BenLeggiero@Gmail.com>
2019-12-10 01:08:42 -0700

    Added `[clamping:]` to `RandomAccessCollection`

--- Tests/SafeCollectionAccessTests/SafeCollectionAccessTests.swift
+++ Tests/SafeCollectionAccessTests/SafeCollectionAccessTests.swift
@@ -67,8 +67,54 @@ final class SafeCollectionAccessTests: XCTestCase {
     }
     
     
+    func testSubscriptClamping() {
+        let first5Fibonacci = [1, 1, 2, 3, 5]
+
+        XCTAssertEqual(1, first5Fibonacci[clamping: 0])
+        XCTAssertEqual(1, first5Fibonacci[clamping: 1])
+        XCTAssertEqual(2, first5Fibonacci[clamping: 2])
+        XCTAssertEqual(3, first5Fibonacci[clamping: 3])
+        XCTAssertEqual(5, first5Fibonacci[clamping: 4])
+        
+        XCTAssertEqual(first5Fibonacci[clamping: -1], first5Fibonacci[clamping: 0])
+        XCTAssertEqual(first5Fibonacci[clamping: 0], first5Fibonacci[clamping: 1])
+        XCTAssertNotEqual(first5Fibonacci[clamping: 1], first5Fibonacci[clamping: 2])
+        XCTAssertNotEqual(first5Fibonacci[clamping: 2], first5Fibonacci[clamping: 3])
+        XCTAssertNotEqual(first5Fibonacci[clamping: 3], first5Fibonacci[clamping: 4])
+        
+        XCTAssertNotNil(first5Fibonacci[clamping: -2])
+        XCTAssertNotNil(first5Fibonacci[clamping: -1])
+        XCTAssertNotNil(first5Fibonacci[clamping: 0])
+        XCTAssertNotNil(first5Fibonacci[clamping: 1])
+        XCTAssertNotNil(first5Fibonacci[clamping: 2])
+        XCTAssertNotNil(first5Fibonacci[clamping: 3])
+        XCTAssertNotNil(first5Fibonacci[clamping: 4])
+        XCTAssertNotNil(first5Fibonacci[clamping: 5])
+        XCTAssertNotNil(first5Fibonacci[clamping: 6])
+        
+        
+        let collectionOfOne = [42]
+        
+        XCTAssertEqual(42, collectionOfOne[clamping: -2])
+        XCTAssertEqual(42, collectionOfOne[clamping: -1])
+        XCTAssertEqual(42, collectionOfOne[clamping: 0])
+        XCTAssertEqual(42, collectionOfOne[clamping: 1])
+        XCTAssertEqual(42, collectionOfOne[clamping: 2])
+        
+        
+        let emptyCollection = [Int]()
+        
+        XCTAssertNil(emptyCollection[clamping: -2])
+        XCTAssertNil(emptyCollection[clamping: -1])
+        XCTAssertNil(emptyCollection[clamping: 0])
+        XCTAssertNil(emptyCollection[clamping: 1])
+        XCTAssertNil(emptyCollection[clamping: 2])
+    }
+    
+    
     static var allTests = [
         ("testSubscriptOrNil", testSubscriptOrNil),
         ("testSubscriptSafe", testSubscriptSafe),
+        ("testSubscriptClamping", testSubscriptClamping),
     ]
 }
