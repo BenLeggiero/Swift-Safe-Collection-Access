5c92e47bb1bb1d578ed625998f86b5f62afd1aec
Ben Leggiero <BenLeggiero@Gmail.com>
2021-06-10 12:53:18 -0600

    Introduced mutation! 🎉
    Existing accessors now behave as mutators as well!
    
    This was much harder than expected 😵

--- Tests/SafeCollectionAccessTests/RangesOrNilTests.swift
+++ Tests/SafeCollectionAccessTests/RangesOrNilTests.swift
@@ -11,9 +11,11 @@ import XCTest
 
 
 
+internal let first5Fibonacci = [1, 1, 2, 3, 5]
+
+
+
 final class RangeOrNilTests: XCTestCase {
-    
-    let first5Fibonacci = [1, 1, 2, 3, 5]
     
     
     func testSubscriptOrNil_Range() {
@@ -57,6 +59,39 @@ final class RangeOrNilTests: XCTestCase {
         XCTAssertNil(first5Fibonacci[orNil:   2 ..<  6])
         XCTAssertNil(first5Fibonacci[orNil: -20 ..< -10])
         XCTAssertNil(first5Fibonacci[orNil:   7 ..<  9])
+        
+        
+        mutationTest { copy1, copy2 in
+            copy2[       1..<3] = [9]
+            copy1[orNil: 1..<3] = [9]
+            
+            XCTAssertEqual(copy1, [1, 9, 3, 5])
+            XCTAssertEqual(copy1, copy2)
+        }
+        
+        mutationTest { copy1, copy2 in
+            copy2[       1..<3] = [7, 8, 9]
+            copy1[orNil: 1..<3] = [7, 8, 9]
+            
+            XCTAssertEqual(copy1, [1, 7, 8, 9, 3, 5])
+            XCTAssertEqual(copy1, copy2)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 0 ..< 5] = [9]
+            XCTAssertEqual(copy, [9])
+        }
+        
+        
+        mutationTest { copy in
+            copy[orNil: -1 ..< 0] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 0 ..< 6] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
     }
     
     
@@ -66,37 +101,96 @@ final class RangeOrNilTests: XCTestCase {
         XCTAssertEqual(first5Fibonacci[orNil: 2...4], [2, 3, 5])
         XCTAssertEqual(first5Fibonacci[orNil: 3...4], [3, 5])
         XCTAssertEqual(first5Fibonacci[orNil: 4...4], [5])
-        
+
         XCTAssertEqual(first5Fibonacci[orNil: 0...4], [1, 1, 2, 3, 5])
         XCTAssertEqual(first5Fibonacci[orNil: 0...3], [1, 1, 2, 3])
         XCTAssertEqual(first5Fibonacci[orNil: 0...2], [1, 1, 2])
         XCTAssertEqual(first5Fibonacci[orNil: 0...1], [1, 1])
         XCTAssertEqual(first5Fibonacci[orNil: 0...0], [1])
-        
+
         XCTAssertEqual(first5Fibonacci[orNil: 2...2], [2])
-        
-        
+
+
         XCTAssertEqual(first5Fibonacci[orNil: 0...4], first5Fibonacci[0...4])
         XCTAssertEqual(first5Fibonacci[orNil: 1...4], first5Fibonacci[1...4])
         XCTAssertEqual(first5Fibonacci[orNil: 2...4], first5Fibonacci[2...4])
         XCTAssertEqual(first5Fibonacci[orNil: 3...4], first5Fibonacci[3...4])
         XCTAssertEqual(first5Fibonacci[orNil: 4...4], first5Fibonacci[4...4])
-        
+
         XCTAssertEqual(first5Fibonacci[orNil: 0...4], first5Fibonacci[0...4])
         XCTAssertEqual(first5Fibonacci[orNil: 0...3], first5Fibonacci[0...3])
         XCTAssertEqual(first5Fibonacci[orNil: 0...2], first5Fibonacci[0...2])
         XCTAssertEqual(first5Fibonacci[orNil: 0...1], first5Fibonacci[0...1])
         XCTAssertEqual(first5Fibonacci[orNil: 0...0], first5Fibonacci[0...0])
-        
+
         XCTAssertEqual(first5Fibonacci[orNil: 2...2], first5Fibonacci[2...2])
-        
-        
+
+
         XCTAssertNil(first5Fibonacci[orNil:  -1 ...  0])
         XCTAssertNil(first5Fibonacci[orNil:  -1 ...  2])
+        XCTAssertNil(first5Fibonacci[orNil:  -1 ...  4])
+        XCTAssertNil(first5Fibonacci[orNil:  -1 ...  5])
+        XCTAssertNil(first5Fibonacci[orNil:   0 ...  5])
+        XCTAssertNil(first5Fibonacci[orNil:   1 ...  5])
+        XCTAssertNil(first5Fibonacci[orNil:   2 ...  5])
+        XCTAssertNil(first5Fibonacci[orNil:   3 ...  5])
+        XCTAssertNil(first5Fibonacci[orNil:   4 ...  5])
+        XCTAssertNil(first5Fibonacci[orNil:   5 ...  5])
         XCTAssertNil(first5Fibonacci[orNil:   0 ...  6])
         XCTAssertNil(first5Fibonacci[orNil:   2 ...  6])
         XCTAssertNil(first5Fibonacci[orNil: -20 ... -10])
         XCTAssertNil(first5Fibonacci[orNil:   7 ...  9])
+        
+        
+        mutationTest { copy1, copy2 in
+            copy2[       1...3] = [9]
+            copy1[orNil: 1...3] = [9]
+            
+            XCTAssertEqual(copy1, [1, 9, 5])
+            XCTAssertEqual(copy1, copy2)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 0 ... 4] = [9]
+            XCTAssertEqual(copy, [9])
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 5 ... 5] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        
+        
+        mutationTest { copy in
+            copy[orNil: -1 ... 0] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: -1 ... 2] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: -20 ... 0] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: -20 ... 2] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 0 ... 5] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 0 ... 6] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
     }
     
     
@@ -107,18 +201,75 @@ final class RangeOrNilTests: XCTestCase {
         XCTAssertEqual(first5Fibonacci[orNil: 3...], [3, 5])
         XCTAssertEqual(first5Fibonacci[orNil: 4...], [5])
         XCTAssertEqual(first5Fibonacci[orNil: 5...], [])
-        
+
         XCTAssertEqual(first5Fibonacci[orNil: 0...], first5Fibonacci[0...])
         XCTAssertEqual(first5Fibonacci[orNil: 1...], first5Fibonacci[1...])
         XCTAssertEqual(first5Fibonacci[orNil: 2...], first5Fibonacci[2...])
         XCTAssertEqual(first5Fibonacci[orNil: 3...], first5Fibonacci[3...])
         XCTAssertEqual(first5Fibonacci[orNil: 4...], first5Fibonacci[4...])
         XCTAssertEqual(first5Fibonacci[orNil: 5...], first5Fibonacci[5...])
-        
+
         XCTAssertNil(first5Fibonacci[orNil: (  6)...])
         XCTAssertNil(first5Fibonacci[orNil: ( -1)...])
         XCTAssertNil(first5Fibonacci[orNil: (-20)...])
         XCTAssertNil(first5Fibonacci[orNil: (  7)...])
+        
+        
+        
+        mutationTest { copy1, copy2 in
+            copy2[       1...] = [9]
+            copy1[orNil: 1...] = [9]
+            
+            XCTAssertEqual(copy1, [1, 9])
+            XCTAssertEqual(copy1, copy2)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 0...] = [9]
+            XCTAssertEqual(copy, [9])
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 1...] = [9]
+            XCTAssertEqual(copy, [1, 9])
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 2...] = [9]
+            XCTAssertEqual(copy, [1, 1, 9])
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 3...] = [9]
+            XCTAssertEqual(copy, [1, 1, 2, 9])
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 4...] = [9]
+            XCTAssertEqual(copy, [1, 1, 2, 3, 9])
+        }
+        
+        mutationTest { copy in
+            copy[orNil: 5...] = [9]
+            XCTAssertEqual(copy, [1, 1, 2, 3, 5, 9])
+        }
+        
+        
+        
+        mutationTest { copy in
+            copy[orNil: 6...] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: (-1)...] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: (-20)...] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
     }
     
     
@@ -141,6 +292,70 @@ final class RangeOrNilTests: XCTestCase {
         XCTAssertNil(first5Fibonacci[orNil: ..<( 6 )])
         XCTAssertNil(first5Fibonacci[orNil: ..<(-10)])
         XCTAssertNil(first5Fibonacci[orNil: ..<( 9 )])
+        
+        
+        
+        mutationTest { copy1, copy2 in
+            copy2[       ..<3] = [9]
+            copy1[orNil: ..<3] = [9]
+            
+            XCTAssertEqual(copy1, [9, 3, 5])
+            XCTAssertEqual(copy1, copy2)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: ..<5] = [9]
+            XCTAssertEqual(copy, [9])
+        }
+        
+        mutationTest { copy in
+            copy[orNil: ..<4] = [9]
+            XCTAssertEqual(copy, [9, 5])
+        }
+        
+        mutationTest { copy in
+            copy[orNil: ..<3] = [9]
+            XCTAssertEqual(copy, [9, 3, 5])
+        }
+        
+        mutationTest { copy in
+            copy[orNil: ..<2] = [9]
+            XCTAssertEqual(copy, [9, 2, 3, 5])
+        }
+        
+        mutationTest { copy in
+            copy[orNil: ..<1] = [9]
+            XCTAssertEqual(copy, [9, 1, 2, 3, 5])
+        }
+        
+        mutationTest { copy1, copy2 in
+            copy1[orNil: ..<0] = [9]
+            copy2[       ..<0] = [9]
+            XCTAssertEqual(copy1, [9, 1, 1, 2, 3, 5])
+            XCTAssertEqual(copy1, copy2)
+        }
+        
+        
+        
+        mutationTest { copy in
+            copy[orNil: ..<6] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: ..<20] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: ..<(-1)] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: ..<(-20)] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
     }
     
     
@@ -150,16 +365,76 @@ final class RangeOrNilTests: XCTestCase {
         XCTAssertEqual(first5Fibonacci[orNil: ...2], [1, 1, 2])
         XCTAssertEqual(first5Fibonacci[orNil: ...1], [1, 1])
         XCTAssertEqual(first5Fibonacci[orNil: ...0], [1])
-        
+
         XCTAssertEqual(first5Fibonacci[orNil: ...4], first5Fibonacci[...4])
         XCTAssertEqual(first5Fibonacci[orNil: ...3], first5Fibonacci[...3])
         XCTAssertEqual(first5Fibonacci[orNil: ...2], first5Fibonacci[...2])
         XCTAssertEqual(first5Fibonacci[orNil: ...1], first5Fibonacci[...1])
         XCTAssertEqual(first5Fibonacci[orNil: ...0], first5Fibonacci[...0])
-        
+
+        XCTAssertNil(first5Fibonacci[orNil: ...( 5 )])
         XCTAssertNil(first5Fibonacci[orNil: ...( 6 )])
         XCTAssertNil(first5Fibonacci[orNil: ...(-10)])
         XCTAssertNil(first5Fibonacci[orNil: ...( 9 )])
+
+
+
+        mutationTest { copy1, copy2 in
+            copy2[       ...3] = [9]
+            copy1[orNil: ...3] = [9]
+
+            XCTAssertEqual(copy1, [9, 5])
+            XCTAssertEqual(copy1, copy2)
+        }
+
+        mutationTest { copy in
+            copy[orNil: ...4] = [9]
+            XCTAssertEqual(copy, [9])
+        }
+
+        mutationTest { copy in
+            copy[orNil: ...3] = [9]
+            XCTAssertEqual(copy, [9, 5])
+        }
+
+        mutationTest { copy in
+            copy[orNil: ...2] = [9]
+            XCTAssertEqual(copy, [9, 3, 5])
+        }
+
+        mutationTest { copy in
+            copy[orNil: ...1] = [9]
+            XCTAssertEqual(copy, [9, 2, 3, 5])
+        }
+
+        mutationTest { copy in
+            copy[orNil: ...0] = [9]
+            XCTAssertEqual(copy, [9, 1, 2, 3, 5])
+        }
+        
+        mutationTest { copy1, copy2 in
+            copy1[orNil: ...(-1)] = [9]
+            copy2[       ...(-1)] = [9]
+            XCTAssertEqual(copy1, [9, 1, 1, 2, 3, 5])
+            XCTAssertEqual(copy1, copy2)
+        }
+        
+        
+        
+        mutationTest { copy in
+            copy[orNil: ...6] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: ...20] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        
+        mutationTest { copy in
+            copy[orNil: ...(-20)] = [9]
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
     }
     
     
@@ -170,4 +445,28 @@ final class RangeOrNilTests: XCTestCase {
         ("testSubscriptOrNil_PartialRangeUpTo", testSubscriptOrNil_PartialRangeUpTo),
         ("testSubscriptOrNil_PartialRangeUpThrough", testSubscriptOrNil_PartialRangeUpThrough),
     ]
+}
+
+
+
+internal func mutationTest<Value>(with value: Value, do closure: (_ copy: inout Value) -> Void) {
+    var copy = value
+    closure(&copy)
+}
+
+
+internal func mutationTest(do test: (_ copy: inout [Int]) -> Void) {
+    mutationTest(with: first5Fibonacci, do: test)
+}
+
+
+internal func mutationTest<Value>(with value: Value, do closure: (_ copy1: inout Value, _ copy2: inout Value) -> Void) {
+    var copy1 = value
+    var copy2 = value
+    closure(&copy1, &copy2)
+}
+
+
+internal func mutationTest(do test: (_ copy1: inout [Int], _ copy2: inout [Int]) -> Void) {
+    mutationTest(with: first5Fibonacci, do: test)
 }
--- Tests/SafeCollectionAccessTests/SafeCollectionAccessTests.swift
+++ Tests/SafeCollectionAccessTests/SafeCollectionAccessTests.swift
@@ -13,10 +13,12 @@ import XCTest
 
 final class SafeCollectionAccessTests: XCTestCase {
     
-    let first5Fibonacci = [1, 1, 2, 3, 5]
+    // MARK: - [orNil:]
     
-    
-    func testSubscriptOrNil() {
+    func testSubscript_orNil() {
+        
+        // MARK: Access
+        
         XCTAssertEqual(1, first5Fibonacci[orNil: 0])
         XCTAssertEqual(1, first5Fibonacci[orNil: 1])
         XCTAssertEqual(2, first5Fibonacci[orNil: 2])
@@ -38,10 +40,106 @@ final class SafeCollectionAccessTests: XCTestCase {
         XCTAssertNotNil(first5Fibonacci[orNil: 4])
         XCTAssertNil(first5Fibonacci[orNil: 5])
         XCTAssertNil(first5Fibonacci[orNil: 6])
+        
+        
+        // MARK: Mutation
+        
+        mutationTest { copy in
+            copy[orNil: 0] = 42
+            XCTAssertEqual(42, copy[orNil: 0])
+            XCTAssertEqual(42, copy[       0])
+            
+            copy[orNil: 0] = nil
+            XCTAssertEqual([1, 2, 3, 5], copy)
+        }
+        mutationTest { copy in
+            copy[orNil: 1] = 42
+            XCTAssertEqual(42, copy[orNil: 1])
+            XCTAssertEqual(42, copy[       1])
+            
+            copy[orNil: 1] = nil
+            XCTAssertEqual([1, 2, 3, 5], copy)
+        }
+        mutationTest { copy in
+            copy[orNil: 2] = 42
+            XCTAssertEqual(42, copy[orNil: 2])
+            XCTAssertEqual(42, copy[       2])
+            
+            copy[orNil: 2] = nil
+            XCTAssertEqual([1, 1, 3, 5], copy)
+        }
+        mutationTest { copy in
+            copy[orNil: 3] = 42
+            XCTAssertEqual(42, copy[orNil: 3])
+            XCTAssertEqual(42, copy[       3])
+            
+            copy[orNil: 3] = nil
+            XCTAssertEqual([1, 1, 2, 5], copy)
+        }
+        mutationTest { copy in
+            copy[orNil: 4] = 42
+            XCTAssertEqual(42, copy[orNil: 4])
+            XCTAssertEqual(42, copy[       4])
+            
+            copy[orNil: 4] = nil
+            XCTAssertEqual([1, 1, 2, 3], copy)
+        }
+        
+        
+        mutationTest { copy in
+            copy[orNil: -20] = 42
+            XCTAssertEqual(copy, first5Fibonacci)
+            
+            copy[orNil: -20] = nil
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        mutationTest { copy in
+            copy[orNil: -2] = 42
+            XCTAssertEqual(copy, first5Fibonacci)
+            
+            copy[orNil: -2] = nil
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        mutationTest { copy in
+            copy[orNil: -1] = 42
+            XCTAssertEqual(copy, first5Fibonacci)
+            
+            copy[orNil: -1] = nil
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        mutationTest { copy in
+            copy[orNil: 5] = 42
+            XCTAssertEqual(copy, first5Fibonacci)
+            
+            copy[orNil: 5] = nil
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        mutationTest { copy in
+            copy[orNil: 6] = 42
+            XCTAssertEqual(copy, first5Fibonacci)
+            
+            copy[orNil: 6] = nil
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
+        mutationTest { copy in
+            copy[orNil: 20] = 42
+            XCTAssertEqual(copy, first5Fibonacci)
+            
+            copy[orNil: 20] = nil
+            XCTAssertEqual(copy, first5Fibonacci)
+        }
     }
     
     
-    func testSubscriptClamping() {
+    
+    // MARK: - [clamping:]
+    
+    // MARK: Multi-elemet arrays
+    
+    func testSubscript_clamping_multiElementArrays() {
+        
+        // MARK: • Access
+        
         XCTAssertEqual(1, first5Fibonacci[clamping: 0])
         XCTAssertEqual(1, first5Fibonacci[clamping: 1])
         XCTAssertEqual(2, first5Fibonacci[clamping: 2])
@@ -65,7 +163,103 @@ final class SafeCollectionAccessTests: XCTestCase {
         XCTAssertNotNil(first5Fibonacci[clamping: 6])
         
         
+        // MARK: • Mutation
+        
+        mutationTest { copy in
+            copy[clamping: 0] = 42
+            XCTAssertEqual(42, copy[clamping: 0])
+            XCTAssertEqual(42, copy[          0])
+            
+            copy[clamping: 0] = nil
+            XCTAssertEqual([1, 2, 3, 5], copy)
+        }
+        mutationTest { copy in
+            copy[clamping: 1] = 42
+            XCTAssertEqual(42, copy[clamping: 1])
+            XCTAssertEqual(42, copy[          1])
+            
+            copy[clamping: 1] = nil
+            XCTAssertEqual([1, 2, 3, 5], copy)
+        }
+        mutationTest { copy in
+            copy[clamping: 2] = 42
+            XCTAssertEqual(42, copy[clamping: 2])
+            XCTAssertEqual(42, copy[          2])
+            
+            copy[clamping: 2] = nil
+            XCTAssertEqual([1, 1, 3, 5], copy)
+        }
+        mutationTest { copy in
+            copy[clamping: 3] = 42
+            XCTAssertEqual(42, copy[clamping: 3])
+            XCTAssertEqual(42, copy[          3])
+            
+            copy[clamping: 3] = nil
+            XCTAssertEqual([1, 1, 2, 5], copy)
+        }
+        mutationTest { copy in
+            copy[clamping: 4] = 42
+            XCTAssertEqual(42, copy[clamping: 4])
+            XCTAssertEqual(42, copy[          4])
+            
+            copy[clamping: 4] = nil
+            XCTAssertEqual([1, 1, 2, 3], copy)
+        }
+        
+        
+        mutationTest { copy in
+            copy[clamping: -20] = 42
+            XCTAssertEqual([42, 1, 2, 3, 5], copy)
+            
+            copy[clamping: -20] = nil
+            XCTAssertEqual([1, 2, 3, 5], copy)
+        }
+        mutationTest { copy in
+            copy[clamping: -2] = 42
+            XCTAssertEqual([42, 1, 2, 3, 5], copy)
+            
+            copy[clamping: -2] = nil
+            XCTAssertEqual([1, 2, 3, 5], copy)
+        }
+        mutationTest { copy in
+            copy[clamping: -1] = 42
+            XCTAssertEqual([42, 1, 2, 3, 5], copy)
+            
+            copy[clamping: -1] = nil
+            XCTAssertEqual([1, 2, 3, 5], copy)
+        }
+        mutationTest { copy in
+            copy[clamping: 5] = 42
+            XCTAssertEqual([1, 1, 2, 3, 42], copy)
+            
+            copy[clamping: 5] = nil
+            XCTAssertEqual([1, 1, 2, 3], copy)
+        }
+        mutationTest { copy in
+            copy[clamping: 6] = 42
+            XCTAssertEqual([1, 1, 2, 3, 42], copy)
+            
+            copy[clamping: 6] = nil
+            XCTAssertEqual([1, 1, 2, 3], copy)
+        }
+        mutationTest { copy in
+            copy[clamping: 20] = 42
+            XCTAssertEqual([1, 1, 2, 3, 42], copy)
+            
+            copy[clamping: 20] = nil
+            XCTAssertEqual([1, 1, 2, 3], copy)
+        }
+    }
+    
+    
+    // MARK: Single-elemet arrays
+    
+    func testSubscript_clamping_singleElementArrays() {
+        
         let collectionOfOne = [42]
+        
+        
+        // MARK: • Access
         
         XCTAssertEqual(42, collectionOfOne[clamping: -2])
         XCTAssertEqual(42, collectionOfOne[clamping: -1])
@@ -74,18 +268,134 @@ final class SafeCollectionAccessTests: XCTestCase {
         XCTAssertEqual(42, collectionOfOne[clamping: 2])
         
         
+        // MARK: • Mutation
+        
+        mutationTest(with: collectionOfOne) { copy in
+            copy[clamping: -20] = 42
+            XCTAssertEqual([42], copy)
+            
+            copy[clamping: -20] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: collectionOfOne) { copy in
+            copy[clamping: -2] = 42
+            XCTAssertEqual([42], copy)
+            
+            copy[clamping: -2] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: collectionOfOne) { copy in
+            copy[clamping: -1] = 42
+            XCTAssertEqual([42], copy)
+            
+            copy[clamping: -1] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: collectionOfOne) { copy in
+            copy[clamping: 0] = 42
+            XCTAssertEqual([42], copy)
+            
+            copy[clamping: 0] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: collectionOfOne) { copy in
+            copy[clamping: 1] = 42
+            XCTAssertEqual([42], copy)
+            
+            copy[clamping: 1] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: collectionOfOne) { copy in
+            copy[clamping: 2] = 42
+            XCTAssertEqual([42], copy)
+            
+            copy[clamping: 2] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: collectionOfOne) { copy in
+            copy[clamping: 20] = 42
+            XCTAssertEqual([42], copy)
+            
+            copy[clamping: 20] = nil
+            XCTAssertEqual([], copy)
+        }
+    }
+    
+    
+    // MARK: Empty arrays
+    
+    func testSubscript_clamping_emptyArrays() {
+        
         let emptyCollection = [Int]()
+        
+        
+        // MARK: • Access
         
         XCTAssertNil(emptyCollection[clamping: -2])
         XCTAssertNil(emptyCollection[clamping: -1])
         XCTAssertNil(emptyCollection[clamping: 0])
         XCTAssertNil(emptyCollection[clamping: 1])
         XCTAssertNil(emptyCollection[clamping: 2])
+        
+        
+        // MARK: • Mutation
+        
+        mutationTest(with: emptyCollection) { copy in
+            copy[clamping: -20] = 42
+            XCTAssertEqual([], copy)
+            
+            copy[clamping: -20] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: emptyCollection) { copy in
+            copy[clamping: -2] = 42
+            XCTAssertEqual([], copy)
+            
+            copy[clamping: -2] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: emptyCollection) { copy in
+            copy[clamping: -1] = 42
+            XCTAssertEqual([], copy)
+            
+            copy[clamping: -1] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: emptyCollection) { copy in
+            copy[clamping: 0] = 42
+            XCTAssertEqual([], copy)
+            
+            copy[clamping: 0] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: emptyCollection) { copy in
+            copy[clamping: 1] = 42
+            XCTAssertEqual([], copy)
+            
+            copy[clamping: 1] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: emptyCollection) { copy in
+            copy[clamping: 2] = 42
+            XCTAssertEqual([], copy)
+            
+            copy[clamping: 2] = nil
+            XCTAssertEqual([], copy)
+        }
+        mutationTest(with: emptyCollection) { copy in
+            copy[clamping: 20] = 42
+            XCTAssertEqual([], copy)
+            
+            copy[clamping: 20] = nil
+            XCTAssertEqual([], copy)
+        }
     }
     
     
     static var allTests = [
-        ("testSubscriptOrNil", testSubscriptOrNil),
-        ("testSubscriptClamping", testSubscriptClamping),
+        ("testSubscript_orNil", testSubscript_orNil),
+        ("testSubscript_clamping_multiElementArrays", testSubscript_clamping_multiElementArrays),
+        ("testSubscript_clamping_singleElementArrays", testSubscript_clamping_singleElementArrays),
+        ("testSubscript_clamping_emptyArrays", testSubscript_clamping_emptyArrays),
     ]
 }
